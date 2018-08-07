2e733edc214ae6e54ec0d741c8850f1f82c98730
0100chf <1536021562@qq.com>
2018-08-07 10:53:54 +0800

    add

--- springboot/sb-shiro-cache/src/main/java/org/spring/springboot/Application.java
+++ springboot/sb-shiro-cache/src/main/java/org/spring/springboot/Application.java
@@ -20,8 +20,8 @@ public class Application {
         // 程序启动入口
         // 启动嵌入式的 Tomcat 并初始化 Spring 环境及其各 Spring 组件
         
-    	//SpringApplication.run(Application.class,args);
-    	printBeans(SpringApplication.run(Application.class,args));
+    	SpringApplication.run(Application.class,args);
+    	//printBeans(SpringApplication.run(Application.class,args));
     }
     
     private static void printBeans(ApplicationContext  ctx){
